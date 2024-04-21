798096af2062e3c8ecf1822960263657188252da
Riya Devikar <145034386+L00179076@users.noreply.github.com>
2024-04-22 00:23:00 +0100

    new vulnerabilities

---
+++ src/main/java/com/example/mavendemo/Vunerabilities.java
@@ -0,0 +1,38 @@
+package com.example.mavendemo;
+
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+
+public class Vunerabilities {
+    String query = "SELECT * FROM users WHERE username = '" + userInput + "'";
+    out.println("<p>Welcome, " + userInput + "!</p>");
+
+    Runtime.getRuntime().exec("ping " + userInput);
+    public Vunerabilities(ObjectInputStream ois) {
+        this.ois = ois;
+    }
+    ObjectInputStream ois = new ObjectInputStream(new FileInputStream("data.ser"));
+    Object obj = ois.readObject();
+    public String getQuery() {
+        return query;
+    }
+    /**
+     * @param query
+     */
+    public void setQuery(String query) {
+        this.query = query;
+    }
+    public ObjectInputStream getOis() {
+        return ois;
+    }
+    public void setOis(ObjectInputStream ois) {
+        this.ois = ois;
+    }
+    public Object getObj() {
+        return obj;
+    }
+    public void setObj(Object obj) {
+        this.obj = obj;
+    }
+    
+}
